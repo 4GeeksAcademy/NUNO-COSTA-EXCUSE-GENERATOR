529f83e57f153d60c5796c21fb0692d1d729c673
Broasdemilho <135333779+Broasdemilho@users.noreply.github.com>
2023-08-07 19:03:23 +0000

    excuse generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  var excuseParts = [
+    ["The dog ", "My grandma ", "His turtle ", "My bird "],
+    ["ate ", "peed ", "crushed ", "broke "],
+    ["my homework ", "the keys ", "the car "],
+    [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ]
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function excuseGenerator(anArray) {
+    let sentence = "";
+    for (let i = 0; i < anArray.length; i++) {
+      let index = Math.floor(Math.random() * anArray[i].length);
+      sentence += anArray[i][index];
+    }
+    return sentence;
+  }
+  document.querySelector("#excuse").innerHTML = excuseGenerator(excuseParts);
 };
